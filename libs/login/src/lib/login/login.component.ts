dfbe8383fe559b57211b113bae9414703f99d875
jonathan_fambro <startek591@gmail.com>
2025-01-06 18:53:30 -0500

    Created the Nx Login Testing Library

---
+++ libs/login/src/lib/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
