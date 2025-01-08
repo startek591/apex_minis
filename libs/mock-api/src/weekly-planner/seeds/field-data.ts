2fe3d504d7963d7845c03542e57bba53e91b95fa
jonathan_fambro <startek591@gmail.com>
2025-01-08 17:02:13 -0500

    Added routes and routing for the login and weekly planner for the mock api library

---
+++ libs/mock-api/src/weekly-planner/seeds/field-data.ts
@@ -0,0 +1,28 @@
+import { FieldData } from '../types';
+
+export const fieldDataSeeds: FieldData = {
+  docks: [
+    {
+      id: 24,
+      name: 'GROCERY',
+    },
+    {
+      id: 40,
+      name: 'DELI',
+    },
+    {
+      id: 4,
+      name: 'FROZEN',
+    },
+  ],
+  shifts: [
+    {
+      id: 15,
+      name: '1ST',
+    },
+    {
+      id: 28,
+      name: '2ND',
+    },
+  ],
+};
---
+++ libs/mock-api/src/weekly-planner/seeds/sub-departments.ts
@@ -0,0 +1,24 @@
+import { SubDepartment } from '../types';
+
+export const subDepartmentSeeds: SubDepartment[] = [
+  {
+    subDept: 10,
+    subDeptName: 'PFS FLORENCE SC',
+    active: true,
+  },
+  {
+    subDept: 11,
+    subDeptName: 'AWG OK CITY',
+    active: true,
+  },
+  {
+    subDept: 12,
+    subDeptName: 'PFS HICKORY NC',
+    active: true,
+  },
+  {
+    subDept: 13,
+    subDeptName: 'INGLES NC',
+    active: true,
+  },
+];
